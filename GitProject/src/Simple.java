2096796ffae9cb99047a00527418a42eee056972
Admin <rohitmaxyadav@gmail.com>
2022-05-15 11:55:26 +0530

    first commit

--- GitProject/src/Simple.java
+++ GitProject/src/Simple.java
@@ -1,13 +1,18 @@
-import java.util.Collections;
-import java.util.TreeSet;
+import java.util.HashSet;
 
-public class Simple {
-	public static void main(String[] args) {
-		TreeSet ts=new TreeSet<>(Collections.reverseOrder());
-		ts.add("Virat");
-		ts.add("msd");
-		ts.add("Rahul");
-		ts.add("Rohit");
-		System.out.println(ts);
-	}
+public class Simple 
+{
+	 public static void main(String[] args) {
+			HashSet <String>hs=new HashSet<>();
+			hs.add("Rohit");
+			hs.add("Rahul");
+			hs.add("Msd");
+			hs.add("Virat");
+			System.out.println(hs);
+			Object x[]=hs.toArray();
+			for(int i=x.length-1;i>=0;i--)
+			{
+				System.out.println(x[i]);
+			}
+		}
 }
